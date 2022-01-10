495fb57dee7ca77d288890cd87bb44898a7f1f81
Nina Reynolds <12169942+cutecycle@users.noreply.github.com>
2022-01-10 14:15:48 -0500

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,31 @@
+$pkgs = @(
+	"Microsoft.dotnet",
+	"Google.Chrome",
+	"vim.vim",
+	"Microsoft.VisualStudioCode",
+	"Microsoft.VisualStudio.2022.Enterprise",
+	"Corsair.iCUE",
+	"Git.Git",
+	"bicep",
+	"Microsoft.PowerShell",
+	"Microsoft.SQLServer.2019.Express"
+	"Microsoft.SQLServerManagementStudio"
+	"Microsoft.AzureDataStudio"
+	"DCSS.DungeonCrawlStoneSoup"
+	"Docker.DockerDesktop"
+	"RedHat.Podman"
+	"Kubernetes.minikube"
+
+
+
+
+)
+
+$pkgs | foreach-object {
+ 	# Start-Job -ScriptBlock { 
+		 winget install -h $_
+	#  } -ArgumentList $_
+}
+
+
+install-Module Az -Force -scope CurrentUser
