3727977aee0a3abc683dac953473657fb09f799b
Nina Reynolds <12169942+cutecycle@users.noreply.github.com>
2022-03-29 14:42:28 -0400

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,79 @@
+$exceptions = "Teams", "powershell", "vscode", "OUTLOOK","iCUE", "Stage"
+
+function endit {
+    $parent = (gwmi win32_process | ? processid -eq  $PID).parentprocessid
+    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -ne "" } | Where-Object { $_.Pid -ne $PID } | Where-Object { $_.Id -ne $parent } | Where-Object { $_.ProcessName -NotIn $exceptions })
+    $procs
+    Stop-Process $procs -ErrorAction SilentlyContinue 
+}
+function k {
+    param(
+        [Parameter(Position = 0)]
+        $name
+    )
+    $parent = (gwmi win32_process | ? processid -eq  $PID).parentprocessid
+    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -like "*$name*" -or $_.ProcessName -like "*$name*" } | Where-Object { $_.MainWindowTitle -ne "" } | Where-Object { $_.Pid -ne $PID } | Where-Object { $_.Id -ne $parent } | Where-Object { $_.ProcessName -NotIn $exceptions })
+    $procs
+    Stop-Process $procs -ErrorAction SilentlyContinue
+}
+
+function cpu { 
+    Get-Process | Sort-Object -Property CPU -Descending | Select-Object -First 5
+}
+function mem { 
+    Get-Process | Sort-Object -Property WS -Descending | Select-Object -First 5
+}
+function cleanup {
+    # Start-Job -ScriptBlock {
+    C:\Users\ninareynolds\Documents\repos\clones\FFIN-MFRR-MSFFR-MFRepo\Infrastructure\Developer\Set-Activation.ps1 -matchExpression "Y-DEV" -justification "clean y-dev"  
+        
+    C:\Users\ninareynolds\Documents\repos\clones\FFIN-MFRR-MSFFR-MFRepo\Infrastructure\Admin\NukeResourceGroups.ps1 -resourceGroupMatchList "Y-DEV"
+        
+
+    # }
+
+}
+
+function Set-Activation { 
+    param ( 
+        [Parameter(Position = 0)]
+        $letter,
+        [Parameter(Position = 1)]
+        $justification
+    )
+    .\infrastructure\developer\set-activation.ps1 -justification $justification -matchExpression "$letter-DEV-COMMON|$letter-DEV-DW|$letter-DEV-ADF"
+}
+
+function brb { 
+    Start-Process "https://i.kym-cdn.com/entries/icons/facebook/000/025/688/maxresdefault.jpg"
+}
+function vsclear { 
+    k code 
+    Remove-Item -Recurse -Force -Path "$Env:APPDATA\Code\Backups"
+}
+
+function Find-AzPortal {
+    param ( 
+        [Parameter(Position = 0)]
+        $name
+    )
+    $url = "https://ms.portal.azure.com/#@microsoft.onmicrosoft.com/resource"
+    $resources = (Get-AzResource -Name *$name*)
+    foreach ($resource in $resources) {
+        Start-Process ($url + $resource.ResourceId)
+    }
+}
+
+function Biglots {
+ k edge
+}
+function Get-ActiveDeployments {
+Get-AzDeployment | Where-Object {$_.ProvisioningState -eq "Running" } 
+}
+
+#$sparkpath = "C:\Users\ninareynolds\Downloads\spark-3.1.2-bin-hadoop3.2"
+$sparkpath = "C:\Users\ninareynolds\Downloads\spark-3.0.1-bin-hadoop2.7"
+$Env:Path = "$($Env:PATH)" + ";C:\Program Files\Vim\vim82" + ";" + $sparkpath
+$Env:HADOOP_HOME = $sparkpath
+$env:SPARK_HOME =  $sparkpath
+# setx /M PATH "%PATH%;%HADOOP_HOME%;%SPARK_HOME%\bin"
