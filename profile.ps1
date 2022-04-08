da0da6ae8889fd1120fb0c7bead59b99b5a38775
Nina Reynolds <12169942+cutecycle@users.noreply.github.com>
2022-04-08 03:24:13 -0400

    asdfiojisfjsdf

--- profile.ps1
+++ profile.ps1
@@ -1,131 +1,132 @@
 $module = New-Module -Name Profile -ScriptBlock {
 
-$exceptions = "Teams", "iCUE"
-$source = "https://raw.githubusercontent.com/cutecycle/dotfiles/master/profile.ps1"
+	$exceptions = "Teams", "iCUE"
+	$source = "https://raw.githubusercontent.com/cutecycle/dotfiles/master/profile.ps1"
 
-# Set-PoshPrompt -theme M365Princess
-function Cache-Command { 
-    param(
-        [Parameter(ValueFromPipeline = $true)]
-        $command
-    )
-    $commandString = $command.ToString()
-    $mystream = [IO.MemoryStream]::new([byte[]][char[]]$commandString)
-    $commandHash = (Get-FileHash -InputStream $mystream -Algorithm SHA256).Hash
-    $target = "~/.cache/$commandHash"
-    if(Test-Path $target){
-        $fresh= (Get-Item -Path $target).CreationTime -lt (Get-Date).AddDays(-1) 
-    }else{
-        $stale = $true
-    }
-    $result = Invoke-Command $command -NoNewScope
-    if ($stale) {
-        Start-ThreadJob { 
-            $result = $using:result
-            $target = $using:target
-            Set-Content -Path $target -Value $result -Force
-        }
-    }
-}
-$azContext = Cache-Command {
-    Get-AzContext
-}
-function Synchronize-Dotfiles {
-    # Stay unidirectional. only edit in codespaces
+	# Set-PoshPrompt -theme M365Princess
+	function Cache-Command { 
+		param(
+			[Parameter(ValueFromPipeline = $true)]
+			$command
+		)
+		$commandString = $command.ToString()
+		$mystream = [IO.MemoryStream]::new([byte[]][char[]]$commandString)
+		$commandHash = (Get-FileHash -InputStream $mystream -Algorithm SHA256).Hash
+		$target = "~/.cache/$commandHash"
+		if (Test-Path $target) {
+			$fresh = (Get-Item -Path $target).CreationTime -lt (Get-Date).AddDays(-1) 
+		}
+		if (-not $fresh) {
+			$result = Invoke-Command $command -NoNewScope
+			Start-ThreadJob { 
+				$result = $using:result
+				$target = $using:target
+				Set-Content -Path $target -Value $result -Force
+			}
+		}
+		else { 
+			$result = Get-Content $target
+		}
+		$result
+	}
+	$azContext = Cache-Command {
+		Get-AzContext
+	}
+	function Synchronize-Dotfiles {
+		# Stay unidirectional. only edit in codespaces
     
-    Start-ThreadJob { 
-        $source = $using:source
-        $PROFILE = $using:PROFILE
-        $content = (Invoke-WebRequest $source).Content
-        $start = Get-Date
+		Start-ThreadJob { 
+			$source = $using:source
+			$profilePath = $using:PROFILE
+			$content = (Invoke-WebRequest $source).Content
            
-            $profileContent= get-content  $PROFILE 
-            $diff=(diff $profileContent $content -ExcludeEqual)
-            if($diff) {
-                Write-Output "Change Detected!"
-                Remove-Item -Force $PROFILE
-             Set-Content -Path $PROFILE -Value $content -Force
-                # Write-Output $diff
-            }
-    }
-}
-function touch { 
-    param(
-        $file
-    )
-    echo $null  >> $file
-}
-function endit {
-    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -ne "" })
-    $procs | Foreach-object -Parallel { 
-        Stop-process $_ -ErrorAction "SilentlyContinue"
-    }
-} 
-function k {
-    param(
-        [Parameter(Position = 0)]
-        $name
-    )
-    $procs = (Get-Process | Where-Object { $_.MainWindowTitle -like "*$name*" -or $_.ProcessName -like "*$name*" })
-    $procs | Foreach-object -Parallel { 
-        Stop-process $_ -ErrorAction "SilentlyContinue"
-    }
-}
+			$profileContent = get-content  $profilePath 
+			$diff = (Compare-Object $profileContent $content)
+			if ($diff) {
+				Write-Output "Change Detected!"
+				Remove-Item -Force $profilePath
+				Set-Content -Path $profilePath -Value $content -Force
+			}
+		}
+	}
+	function touch { 
+		param(
+			$file
+		)
+		echo $null  >> $file
+	}
+	function endit {
+		$procs = (Get-Process | Where-Object { $_.MainWindowTitle -ne "" })
+		$procs | Foreach-object -Parallel { 
+			Stop-process $_ -ErrorAction "SilentlyContinue"
+		}
+	} 
+	function k {
+		param(
+			[Parameter(Position = 0)]
+			$name
+		)
+		$procs = (Get-Process | Where-Object { $_.MainWindowTitle -like "*$name*" -or $_.ProcessName -like "*$name*" })
+		$procs | Foreach-object -Parallel { 
+			Stop-process $_ -ErrorAction "SilentlyContinue"
+		}
+	}
 
-function cpu { 
-    Get-Process | Sort-Object -Property CPU -Descending | Select-Object -First 5
-}
-function mem { 
-    Get-Process | Sort-Object -Property WS -Descending | Select-Object -First 5
-}
+	function cpu { 
+		Get-Process | Sort-Object -Property CPU -Descending | Select-Object -First 5
+	}
+	function mem { 
+		Get-Process | Sort-Object -Property WS -Descending | Select-Object -First 5
+	}
 
-function brb { 
-    Start-Process "https://i.kym-cdn.com/entries/icons/facebook/000/025/688/maxresdefault.jpg" -WindowStyle Maximized
-}
-function vsclear { 
-    k code 
-    Remove-Item -Recurse -Force -Path "$Env:APPDATA\Code\Backups"
-}
-function Find-AzPortal {
-    param ( 
-        [Parameter(Position = 0)]
-        $name
-    )
-    $url = "https://$($azContext.Environment.ManagementPortalUrl)/#$(($azContext).Tenant.Id))/resource"
-    $resources = (Get-AzResource -Name *$name*)[0..5]
-    $resources | ForEach-Object { 
+	function brb { 
+		Start-Process "https://i.kym-cdn.com/entries/icons/facebook/000/025/688/maxresdefault.jpg" -WindowStyle Maximized
+	}
+	function vsclear { 
+		k code 
+		Remove-Item -Recurse -Force -Path "$Env:APPDATA\Code\Backups"
+	}
+	function Find-AzPortal {
+		param ( 
+			[Parameter(Position = 0)]
+			$name
+		)
+		$url = "https://$($azContext.Environment.ManagementPortalUrl)/#$(($azContext).Tenant.Id))/resource"
+		$resources = (Get-AzResource -Name *$name*)[0..5]
+		$resources | ForEach-Object { 
         ($url + $resource.ResourceId)
-    }
-    | ForEach-Object -Parallel { 
-        Start-Process $_
-    }
+		}
+		| ForEach-Object -Parallel { 
+			Start-Process $_
+		}
     
-}
+	}
 
-function Biglots {
-    k edge
-}
-function Get-Deployments {
-    Get-AzDeployment | Where-Object { $_.ProvisioningState -eq "Running" } 
-}
+	function Biglots {
+		k edge
+	}
+	function Get-Deployments {
+		Get-AzDeployment | Where-Object { $_.ProvisioningState -eq "Running" } 
+	}
 }
 Import-Module $module
 # Set-PoshPrompt -theme stelbent.minimal
 
 $extras = @(
-    ";~\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts",
-    ";C:\Program Files\Vim\vim82"
+	";~\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\LocalCache\local-packages\Python310\Scripts",
+	";C:\Program Files\Vim\vim82"
 )
 $env:PATH += ($extras | Join-String)
-$jobs=@()
+$jobs = @()
 function prompt {
-    $str = $jobs | Foreach-Object { 
+	$str = $jobs | Foreach-Object { 
         (($_.status -eq "Completed") ? "✅": "♻️")
-    }
-    $str += ($jobs.length + "jobs/")
-    $str += $pwd
-    $str += ">"
-    $str
-    $jobs | Receive-Job -AutoRemoveJob -WriteEvents
-    $jobs += (Synchronize-Dotfiles)
+	}
+	$str += ($jobs.length + "jobs/")
+	$str += $pwd
+	$str += ">"
+	$str
+	$jobs | Receive-Job -AutoRemoveJob -WriteEvents
+	$jobs += (Synchronize-Dotfiles)
 }
+
